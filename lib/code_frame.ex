b6e21a5fa921a674a7071594f65da1fd98f67a7b
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-11-19 21:31:38 +0100

    Add code frame draft

---
+++ lib/code_frame.ex
@@ -0,0 +1,74 @@
+defmodule CodeFrame do
+  alias IO.ANSI
+
+  @moduledoc """
+  Documentation for CodeFrame.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodeFrame.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @type options :: []
+
+  @spec build(String.t(), integer, options) :: String.t()
+  def build(input, line_pos, options \\ []) do
+    lines = input |> String.split("\n")
+    count = length(lines)
+
+    # Parse Options
+    shouldHighlight = Keyword.get(options, :highlight, false)
+    l_before = max(line_pos - Keyword.get(options, :lines_before, 2), 0)
+    l_after = min(line_pos + Keyword.get(options, :lines_after, 2), count)
+
+    lines
+    |> Enum.with_index()
+    |> Enum.filter(fn {_, i} -> i >= l_before and i <= l_after end)
+    |> Enum.map(fn {x, i} -> print_column(x, i + 1, i == line_pos, shouldHighlight, count) end)
+    |> Enum.join("\n")
+  end
+
+  defp print_column(content, line, active, highlight, total) do
+    diff = length(Integer.digits(total, 10)) - length(Integer.digits(line, 10))
+    indent = String.duplicate(" ", diff)
+
+    if highlight do
+      print_column_colors(content, line, active, indent)
+    else
+      print_column_raw(content, line, active, indent)
+    end
+  end
+
+  defp print_column_colors(content, line, active, indent) do
+    if active do
+      # Code line
+      prefix = ANSI.format([:bright, :red, "> ", indent, to_string(line), " | "])
+      code = ANSI.format([:light_red_background, :black, " ", content, " ", ANSI.reset()])
+
+      [prefix, code, ANSI.reset()]
+    else
+      prefix = ANSI.format([:light_black, "  ", indent, to_string(line), " |  "])
+      ANSI.format([prefix, :default_color, content, ANSI.reset()])
+    end
+  end
+
+  defp print_column_raw(content, line, active, indent) do
+    if active do
+      # Code line
+      second_line = [" | ", String.duplicate("^", String.length(content))]
+      indent2 = String.duplicate(" ", length(Integer.digits(line, 10)))
+      ["> ", to_string(line), " | ", content, "\n  ", indent2, second_line]
+    else
+      ["  ", indent, to_string(line), " | ", content]
+    end
+  end
+end
---
+++ test/code_frame_test.exs
@@ -0,0 +1,42 @@
+defmodule CodeFrameTest do
+  use ExUnit.Case
+  alias IO.ANSI
+
+  doctest CodeFrame
+
+  test "prints before" do
+    res = CodeFrame.build("foo\nbar\nbob", 1)
+    assert res == "  1 | foo\n> 2 | bar\n    | ^^^\n  3 | bob"
+  end
+
+  test "respect line before and after" do
+    res = CodeFrame.build("foo\nbar\nbob\nboof", 2, lines_after: 1, lines_before: 1)
+    assert res == "  2 | bar\n> 3 | bob\n    | ^^^\n  4 | boof"
+  end
+
+  test "prints colors" do
+    res = CodeFrame.build("foo\nbar\nbob\nboof", 1, highlight: true)
+    assert res =~ ANSI.red()
+  end
+
+  test "indent without colors" do
+    res =
+      CodeFrame.build(
+        "foo\nbar\nbob\nboof\nfos\nasd\nasd\nasd\nasd\nasd\nasd\nasd",
+        9
+      )
+
+    assert String.starts_with?(res, "  ")
+  end
+
+  test "indent with colors" do
+    res =
+      CodeFrame.build(
+        "foo\nbar\nbob\nboof\nfos\nasd\nasd\nasd\nasd\nasd\nasd\nasd",
+        9,
+        highlight: true
+      )
+
+    assert String.starts_with?(res, ANSI.light_black() <> "  ")
+  end
+end
